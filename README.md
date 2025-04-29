53e3dca062afb7c544534fa71516a8c481593f32
ionajo <24511059+ionajo@users.noreply.github.com>
2025-04-29 11:45:07 -0400

    add point cloud viewer

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # woodford-county-change
 Woodford County change from 2010 to 2020
+[Look at this point cloud](potree)
---
+++ potree/pointclouds/index/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 4695461,
+    "boundingBox": {
+        "lx": 5208145.64,
+        "ly": 3902993.5500000005,
+        "lz": 847.9200000000001,
+        "ux": 5212145.63,
+        "uy": 3906993.5400000007,
+        "uz": 4847.910000000224
+    },
+    "tightBoundingBox": {
+        "lx": 5208145.64,
+        "ly": 3902993.5500000005,
+        "lz": 847.9200000000001,
+        "ux": 5212145.63,
+        "uy": 3906993.54,
+        "uz": 1080.56
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 34.64093017578125,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
